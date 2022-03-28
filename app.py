c3e2f68289719359a6bb392e79df89a1da3fbb6f
troye <troyegilbert@pm.me>
2022-03-28 16:30:20 +0300

    heroku deployment

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import flask
 import newspaper
 import joblib
 
-
 from flask import Flask, request, render_template
 from flask_cors import CORS
 from newspaper import Article
@@ -37,7 +36,7 @@ def predict():
     
 #Passing the news article to the model and returing whether it is Fake or Real
     pred = model.predict([news])
-    return render_template('main.html', prediction_text='The news is "{}"'.format(pred[0]))
+    return render_template('main.html', prediction_text='The source of this news is "{}"'.format(pred[0]))
     
 if __name__=="__main__":
     port=int(os.environ.get('PORT',5000))
